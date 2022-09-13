f27504b3a80621d4b895340feeead88581e389ef
100128469 <parth.mittal0115@outlook.com>
2022-09-13 09:47:51 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,49 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+        byte computer = 111;
+        short hinder = -1234;
+        int nutella = -450328234;
+        long java = 214339487L;
+        float party = 34507;
+        double mansion = 23498237.69;
+        char vanish = 'f' + 20;
+        boolean black = false;
+        String home = "Nelson Mandela";
+
+        System.out.println(computer);
+        System.out.println(hinder);
+        System.out.println(nutella);
+        System.out.println(java);
+        System.out.println(party);
+        System.out.println(mansion);
+        System.out.println(vanish);
+        System.out.println(black);
+        System.out.println(home);
+
+        System.out.println("/////////////////////////////////");
+        System.out.print(home);
+        System.out.println("         08/26/2022");
+        System.out.println("*" + nutella  +  "*");
+        System.out.println("*                               *");
+        System.out.println(238 - computer);
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+    }
+}
